29b619acbce74831b1506f6cbceeb8ad20e94c67
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-03 03:58:20 +0100

    Add colors to infos

--- js/writeOsInfo.js
+++ js/writeOsInfo.js
@@ -1,15 +1,16 @@
 const os = require("os");
-const changeTimeFormat = require('./changeTimeFormat');
+const colors = require("colors");
+const changeTimeFormat = require("./changeTimeFormat");
 
 function writeOsInfo() {
-	console.log("Platform: " + os.platform());
-	console.log("Relase: " + os.release());
-	console.log("CPU: " + os.cpus()[0].model);
+	console.log("Platform: ".grey + os.platform());
+	console.log("Relase: ".red + os.release());
+	console.log("CPU: ".blue + os.cpus()[0].model);
 
 	let time = changeTimeFormat.change(os.uptime());
 
-	console.log("Uptime: " + time);
-	console.log("Username: " + os.userInfo().username);
+	console.log("Uptime: ".green + time);
+	console.log("Username: ".yellow + os.userInfo().username);
 	console.log("User home dir: " + os.userInfo().homedir);
 }
 
